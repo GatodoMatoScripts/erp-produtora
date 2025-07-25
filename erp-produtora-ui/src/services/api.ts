e5d003546425d72ddaa0817fb1b0566043b13d5f
Gato Scripts <janilsonlima@gmail.com>
2025-07-25 21:58:43 +0000

    Configura ambiente com Docker Compose para o Gitpod

--- erp-produtora-ui/src/services/api.ts
+++ erp-produtora-ui/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL, // <-- MUDANÇA AQUI
 });
 
 api.interceptors.request.use((config: AxiosRequestConfig) => {
